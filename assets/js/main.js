97ff02974bff76fa9822760ed8642c871f085d1b
Dnomd343 <dnomd343@163.com>
2021-04-23 11:53:12 +0800

    update: frontend IP detection, mapbox files localization

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,41 @@
-$(document).ready(function () {
-    $.get("/ip", function (data) {
+$(document).ready(function() {
+    $.get("/ip", function(data) {
         $("#ip_default").val(data);
         getInfo();
     });
     $("table").hide();
-    $("button").click(function () {
+    $("button").click(function() {
+        $("button").css({
+            'border-color': '',
+            'background-color': ''
+        });
         $("button").text("Searching...");
         $("table").hide(1000);
-        getInfo();
+        if (checkIP($("input").val()) == "ok") {
+            getInfo();
+        } else {
+            errorIP();
+        }
     });
 });
-$(document).keydown(function (event) {
+
+$(document).keydown(function(event) {
     if (event.keyCode == 13) {
         $("button").click();
     }
 });
+
 function getInfo() {
-    $.get("/info/" + $("input").val(), function (data) {
+    $.get("/info/" + $("input").val(), function(data) {
+        console.log(data);
         if (data.status == "F") {
-            $("button").text("Illegal IP");
-            $("button").css({ 'border-color': '#ff406f', 'background-color': '#ff406f' });
-            return 0;
+            errorIP();
+            return;
         }
         if (!$("input").val()) {
             $("input").val(data.ip);
         }
         $("button").text("Search");
-        $("button").css({ 'border-color': '', 'background-color': '' });
         $("table").show(1000);
         $("#ip").text(data.ip);
         $("#as").text(data.as);
@@ -34,15 +43,36 @@ function getInfo() {
         $("#region").text(data.region);
         $("#country").text(data.country);
         $("#timezone").text(data.timezone);
-        var earth = "https://earth.google.com/web/@" + data.loc + ",0a,398836d,1y,0h,0t,0r";
-        $("#loc").html('<a  href=' + earth + 'target="_blank">' + data.loc + '</a>');
+        var earthUri = "https://earth.google.com/web/@" + data.loc + ",0a,398836d,1y,0h,0t,0r";
+        $("#loc").html('<a href="' + earthUri + '" target="_blank" title="On Google Earth">' + data.loc + '</a>');
         $("#isp").text(data.isp);
         $("#scope").text(data.scope);
         $("#detail").text(data.detail);
         draw(parseFloat(data.loc.split(',')[0]), parseFloat(data.loc.split(',')[1]));
-
-
     });
+}
+
+function errorIP() {
+    $("button").text("Illegal IP");
+    $("button").css({
+        'border-color': '#ff406f',
+        'background-color': '#ff406f'
+    });
+}
+
+function checkIP(ipStr) {
+    if (ipStr === null) {
+        return "error";
+    }
+    var regIPv4=/^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$/;
+    var regIPv6=/^((([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){1,7}:)|(([0-9A-Fa-f]{1,4}:){6}:[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){5}(:[0-9A-Fa-f]{1,4}){1,2})|(([0-9A-Fa-f]{1,4}:){4}(:[0-9A-Fa-f]{1,4}){1,3})|(([0-9A-Fa-f]{1,4}:){3}(:[0-9A-Fa-f]{1,4}){1,4})|(([0-9A-Fa-f]{1,4}:){2}(:[0-9A-Fa-f]{1,4}){1,5})|([0-9A-Fa-f]{1,4}:(:[0-9A-Fa-f]{1,4}){1,6})|(:(:[0-9A-Fa-f]{1,4}){1,7})|(([0-9A-Fa-f]{1,4}:){6}(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3})|(([0-9A-Fa-f]{1,4}:){5}:(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3})|(([0-9A-Fa-f]{1,4}:){4}(:[0-9A-Fa-f]{1,4}){0,1}:(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3})|(([0-9A-Fa-f]{1,4}:){3}(:[0-9A-Fa-f]{1,4}){0,2}:(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3})|(([0-9A-Fa-f]{1,4}:){2}(:[0-9A-Fa-f]{1,4}){0,3}:(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3})|([0-9A-Fa-f]{1,4}:(:[0-9A-Fa-f]{1,4}){0,4}:(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3})|(:(:[0-9A-Fa-f]{1,4}){0,5}:(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3}))$/;
+    var V4 = ipStr.match(regIPv4);
+    var V6 = ipStr.match(regIPv6);
+    if (V4 === null && V6 === null) {
+        return "error";
+    } else {
+        return "ok";
+    }
 }
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2hldm9ua3VhbiIsImEiOiJja20yMjlnNDYybGg2Mm5zNW40eTNnNnUwIn0.6xj6sgjWvdQgT_7OQUy_Jg';
@@ -62,14 +92,14 @@ function draw(x, y) {
         height: size,
         data: new Uint8Array(size * size * 4),
 
-        onAdd: function () {
+        onAdd: function() {
             var canvas = document.createElement('canvas');
             canvas.width = this.width;
             canvas.height = this.height;
             this.context = canvas.getContext('2d');
         },
 
-        render: function () {
+        render: function() {
             var duration = 1000;
             var t = (performance.now() % duration) / duration;
 
@@ -104,7 +134,7 @@ function draw(x, y) {
         }
     };
 
-    map.on('load', function () {
+    map.on('load', function() {
         map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
         map.addLayer({
             "id": "points",
